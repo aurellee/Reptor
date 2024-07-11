2f6cbfd2dedeb9828ad80520abdaa1ee1dedc474
aurellee <165924639+aurellee@users.noreply.github.com>
2024-07-11 16:40:04 +0700

    Add files via upload

---
+++ Reptor/Reptor/ReptorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReptorApp.swift
+//  Reptor
+//
+//  Created by Jaqueline Aurelia Langi on 11/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ReptorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
